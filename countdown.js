1584fdc8be5513b54a64a4d4e521cfa2bc97b1db
Eduardo Correia <44172778+Educorreia932@users.noreply.github.com>
2019-09-19 12:36:16 +0100

    Date fixed

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 21, 2024").getTime();
+var countDownDate = new Date("Jun 21, 2023").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
