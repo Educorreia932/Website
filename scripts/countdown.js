5314b7e7f00891a1c06a4fc0d5d07d9207ea5dea
Eduardo <skelozard1@gmail.com>
2020-01-12 00:30:09 +0000

    Re-organized structure

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -16,11 +16,11 @@ var x = setInterval(function() {
 	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 	// Display the result in the element with id="demo"
-	document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+	document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
 	// If the count down is finished, write some text 
 	if (distance < 0) {
 		clearInterval(x);
-		document.getElementById("demo").innerHTML = "Finished";
+		document.getElementById("countdown").innerHTML = "Finished";
 	}
 }, 1000);
