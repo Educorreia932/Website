5dde4ac5d07fc8ef46dce301034a3eb14c2ec513
Educorreia932 <skelozard1@gmail.com>
2020-06-28 02:38:19 +0100

    Added the visited countries globe to the about me page
    And fixed an error where Belgium wouldn't be highlighted because of leading zeros in its ID

--- scripts/globe.js
+++ scripts/globe.js
@@ -1,12 +1,12 @@
-var width = 600,
-    height = 500,
-    sens = 0.25,
+var width = 350,
+    height = 350,
+    sens = 0.40,
     focused,
     data;
 
 //Setting projection
 var projection = d3.geoOrthographic()
-    .scale(245)
+    .scale(150)
     .rotate([0, 0])
     .translate([width / 2, height / 2])
     .clipAngle(90);
@@ -30,7 +30,7 @@ var countryList = d3.select("body").append("select").attr("name", "countries");
 queue()
     .defer(d3.json, 'https://unpkg.com/world-atlas@1/world/110m.json')
     .defer(d3.tsv, "https://raw.githubusercontent.com/KoGor/Map-Icons-Generator/master/data/world-110m-country-names.tsv")
-    .defer(d3.csv, "https://gist.githubusercontent.com/Educorreia932/677a39978af8a5983150df5d107942ba/raw/981cdce7fa1a51354d9d6e0721ae772983d50119/visited-countries.csv", data)
+    .defer(d3.csv, "https://gist.githubusercontent.com/Educorreia932/677a39978af8a5983150df5d107942ba/raw/5152b1cae2d866ad64bf58395153e36b89672a9f/visited-countries.csv", data)
     .await(ready);
 
 //Main function
@@ -38,7 +38,7 @@ function ready(error, world, countryData, data) {
     var countryById = {};
     var countries = topojson.feature(world, world.objects.countries).features;
 
-    //Adding countries to select
+    // Adding countries to select
     countryData.forEach(function (d) {
         countryById[d.id] = d.name;
         option = countryList.append("option");
@@ -46,7 +46,7 @@ function ready(error, world, countryData, data) {
         option.property("value", d.id);
     });
 
-    //Drawing countries on the globe
+    // Drawing countries on the globe
     var world = svg.selectAll("path.land")
         .data(countries)
         .enter().append("path")
@@ -54,17 +54,19 @@ function ready(error, world, countryData, data) {
         .attr("d", path)
         .each(function(d, i) {
             try {
+                d.id = d.id.replace(/^0+/, ''); // Removes leading zeros
                 country_name = countryById[d.id].split(' ').join('_');
             }
 
             catch(err) {
+                console.log(d.id)
                 country_name = "undefined"
             }
             
             d3.select(this).attr("id", country_name);
         })
 
-        //Mouse events
+        // Mouse events
         .on("mouseover", function (d) {
             countryTooltip.text(countryById[d.id.replace(/^0+/, '')])
                 .style("left", (d3.event.pageX + 7) + "px")
