7a177f6536480bf82938516258a6a1a1fa416e7b
root <skelozard1@gmail.com>
2020-06-29 22:08:41 +0000

    First verstion of collage done

---
+++ php/save_stamp.php
@@ -0,0 +1,51 @@
+<?php
+    function goBack() {
+        header("Location: https://educorreia932.dev/html/collage.html");
+        die();
+    }
+
+    $url = $_POST["url"];
+
+    // Check if the link corresponds to an image
+
+    $headers = get_headers($url, 1);
+    
+    if (strpos($headers['Content-Type'], 'image/') === false)
+        goBack();
+    
+    $x = $_POST["x"];
+    $y = $_POST["y"];
+
+    // Invalid coordinates
+
+    if ($x > 1000 || $y > 1000)
+        goBack();
+
+    // See if image was already uploaded
+
+    $matches = array();
+
+    $handle = @fopen("../data/stamps.txt", "r");
+
+    if ($handle) {
+        while (!feof($handle)) {
+            $buffer = fgets($handle);
+
+            if (strpos($buffer, $url . " ") !== FALSE)
+                $matches[] = $buffer;
+        }
+
+        fclose($handle);
+    }
+
+    if (sizeof($matches) != 0) {
+        header("Location: https://educorreia932.dev/html/collage.html");
+        die();
+    }
+
+    $stamp = $url . " " . $x . " " . $y . "\n";
+
+    file_put_contents("../data/stamps.txt", $stamp, FILE_APPEND);
+
+    goBack();
+?>
---
+++ scripts/display-stamps.js
@@ -0,0 +1,17 @@
+
+fetch('../data/stamps.txt')
+    .then(response => response.text())
+    .then(text => {
+        var content = text.split("\n");
+
+        for (var i = 0; i < content.length; i++) {
+            var line = content[i].split(" ");
+
+            var img = document.createElement("img");
+            img.src = line[0];
+            img.style.left = line[1] + "px";
+            img.style.top = line[2] + "px";
+
+            document.getElementById("canvas").appendChild(img);
+        }
+    })
