1408209771e13dd0b25cf0a538333994763ffc5d
Eduardo Correia <skelozard1@gmail.com>
2021-10-02 17:23:51 +0100

    Server fetching projects data

--- src/prisma/projects.js
+++ src/prisma/projects.js
@@ -2,13 +2,202 @@ module.exports = {
     projects: [
         {
             title: "Every Web is a Home",
-            project_url: "Game developed for Global Game Jam 2019 in which you are a spider trying to catch bugs with your web.",
+            description: "Game developed for Global Game Jam 2019 in which you are a spider trying to catch bugs with your web.",
+            project_url: "https://github.com/Lantaros/pouca-teia",
+            image: "",
+            tags: {
+                create: [
+                    {name: "Game"},
+                    {name: "Game-Jam"}
+                ]
+            }
+        },
+        {
+            title: "Github Pie Chart",
+            description: "A script that generates a pie chart with the lines of code percentage of each language of a GitHub profile.",
+            project_url: "https://github.com/Educorreia932/Github-Pie-Chart",
+            image: "",
+            tags: {
+                create: [
+                    {name: "Chart"},
+                    {name: "Statistics"},
+                ]
+            }
+        },
+        {
+            title: "Project Watt",
+            description: "A platforming game themed around an electric particle in a circuit trying to reach the exit (connect to earth) and developed for MINIX.",
+            project_url: "https://github.com/Educorreia932/LCOM/tree/master/proj",
+            image: "",
+            tags: {
+                create: [
+                    {name: "Game"},
+                    {name: "University"},
+                ]
+            }
+        },
+        {
+            title: "Lain",
+            description: "A Discord bot that retrieves statistics for a text channel.",
+            project_url: "https://github.com/Educorreia932/Lain",
+            image: "",
+            tags: {
+                create: [
+                    {name: "Bot"}
+                ]
+            }
+        },
+        {
+            title: "CTM 2020 Summer Internship",
+            description: "Flying communications node controller sensitive to traffic.",
+            project_url: "https://github.com/Educorreia932/CTM-2020-Summer-Intership",
+            image: "",
+            tags: {}
+        },
+        {
+            title: "Pokémon Origins",
+            description: "An infographics on pokémon and its counterpart inspirational origins in flora and fauna.",
+            project_url: "https://github.com/Educorreia932/Pokemon-Origins",
+            image: "",
+            tags: {
+                create: [
+                    {name: "Chart"}
+                ]
+            }
+        },
+        {
+            title: "Pokémon - A Sequel Coming In Inevitably",
+            description: "Pokémon Fire Red look-alike running on a terminal.",
+            project_url: "https://github.com/Educorreia932/LPOO/tree/master/Project",
+            image: "",
+            tags: {
+                create: [
+                    {name: "Game"},
+                    {name: "University"},
+                ]
+            }
+        },
+        {
+            title: "McDelivery",
+            description: "WebGL scene redemption of a McDonald's zeppelin delivering hamburgers.",
+            project_url: "https://educorreia932.github.io/CGRA/Project/src/",
+            image: "",
+            tags: {
+                create: [
+                    {name: "University"},
+                ]
+            }
+        },
+        {
+            title: "Tour Mate App",
+            description: "An application that let's you plan a tour around Porto by choosing several touristic attractions and calculating an optimal route to let you visit them.",
+            project_url: "https://github.com/Educorreia932/CAL/tree/master/Project",
+            image: "",
+            tags: {
+                create: [
+                    {name: "University"},
+                ]
+            }
+        },
+        {
+            title: "Oblivion Visual Studio Code Theme",
+            description: "A syntax coloring theme for Visual Studio Code based on the already existing Oblivion.",
+            project_url: "https://github.com/Educorreia932/Oblivion-Theme",
+            image: "",
+            tags: {
+                create: [
+                    {name: "Theme"}
+                ]
+            }
+        },
+        {
+            title: "Ask Away",
+            description: "A mobile application designed to ask questions in conference talks.",
+            project_url: "https://github.com/Educorreia932/ESOF",
+            image: "",
+            tags: {
+                create: [
+                    {name: "University"}
+                ]
+            }
+        },
+        {
+            title: "Umbrella",
+            description: "Game developed for Global Game Jam 2021.",
+            project_url: "https://globalgamejam.org/2021/games/umbrella-4",
+            image: "",
+            tags: {
+                create: [
+                    {name: "Game"},
+                    {name: "Game-Jam"}
+                ]
+            }
+        },
+        {
+            title: "Break_In",
+            description: "Game developed for Retro Jam 2021.",
+            project_url: "https://github.com/xavier-santos/BreakIn",
+            image: "",
+            tags: {
+                create: [
+                    {name: "Game"},
+                    {name: "Game-Jam"}
+                ]
+            }
+        },
+        {
+            title: "Bad Apple",
+            description: "My redemptions of the popular Touhou animated video.",
+            project_url: "https://github.com/Educorreia932/Bad-Apple",
+            image: "",
+            tags: {}
+        },
+        {
+            title: "Spotify Statistics",
+            description: "A webpage that gives you some insight regarding your favorite music genres according to what you listen to on Spotify.",
+            project_url: "https://educorreia932.dev/spotify_statistics",
+            image: "",
+            tags: {
+                create: [
+                    {name: "Chart"}
+                ]
+            }
+        },
+        {
+            title: "Trade-a-Bid",
+            description: "Online auction platform for users to buy and sell all kinds of digital products, through a bidding system.",
+            project_url: "https://github.com/Educorreia932/LBAW",
+            image: "",
+            tags: {
+                create: [
+                    {name: "Website"}
+                ]
+            }
+        },
+        {
+            title: "Undermountain",
+            description: "A roguelike game based on the Undermountain setting of Dungeons & Dragons.",
+            project_url: "https://github.com/Educorreia932/Undermountain",
             image: "",
             tags: {
                 create: [
                     {name: "Game"}
                 ]
             }
-        }
+        },
+        {
+            title: "PokéBattle",
+            description: "An esoteric language designed so that the program looks like the transcript of a Pokémon battle.",
+            project_url: "https://github.com/Educorreia932/PokeBattle",
+            image: "",
+            tags: {}
+        },
+        {
+            title: "Persona 4 - Weather News ",
+            description: "A Google Chrome extension that makes your New Tab page display the weather news in the style of Persona 4.",
+            project_url: "https://github.com/Educorreia932/Persona-4-Weather-News",
+            image: "",
+            tags: {}
+        },
     ]
 }
