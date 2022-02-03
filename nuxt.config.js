f50915e394155d628e64d2dd747982bc25dcaab7
Eduardo Correia <skelozard1@gmail.com>
2022-02-03 11:52:36 +0000

    Style tweaks
    - Enabled Tailwind JIT
    - Purging CSS
    - Extracting CSS to a file

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,7 @@ export default {
 	modules: [],
 	build: {
 		standalone: true,
+		extractCSS: true
 	},
 	vue: {
 		config: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
-	purge: false,
+	purge: {
+		content: [
+			`components/**/*.{vue,js}`,
+			`layouts/**/*.vue`,
+			`pages/**/*.vue`,
+			`plugins/**/*.{js,ts}`,
+			`nuxt.config.{js,ts}`,
+		],
+	},
 	darkMode: "class",
+	mode: "jit",
 	theme: {
 		darkSelector: ".dark-mode",
 		extend: {
