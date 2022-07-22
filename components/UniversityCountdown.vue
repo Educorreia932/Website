7a66f8d1ae6040f978df2caf5025f14aefa5f73f
Educorreia932 <skelozard1@gmail.com>
2022-07-23 00:47:29 +0100

    Rework on homepage
    - Merged ProgressBar and Countdown into UniversityCountdown

---
+++ components/UniversityCountdown.vue
@@ -0,0 +1,49 @@
+<template>
+	<div id="progress-bar">
+		<span id="after" :style="{ width: `${barWidth * 100}%` }"></span>
+	</div>
+
+	<p id="countdown">
+		<template v-for="(value, key, i) in remaining" :key="i">
+			{{ Math.floor(value) }}{{ key[0] }}{{" "}}
+		</template>
+
+		left until I end my university course
+	</p>
+</template>
+
+<script setup lang="ts">
+const startDate = new Date(2018, 9, 8);
+const endDate = new Date(2023, 6, 21);
+const remainingMiliseconds = ref(endDate.getTime() - (new Date()).getTime());
+
+const remaining = computed(() => ({
+	days: remainingMiliseconds.value / (1000 * 60 * 60 * 24),
+	hours: (remainingMiliseconds.value % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+	minutes: (remainingMiliseconds.value % (1000 * 60 * 60)) / (1000 * 60),
+	seconds: (remainingMiliseconds.value % (1000 * 60)) / 1000
+}));
+
+const barWidth = computed(() => 1 - (remainingMiliseconds.value / (endDate.getTime() - startDate.getTime())));
+
+setInterval(() => {
+	remainingMiliseconds.value -= 1000;
+}, 1000);
+</script>
+
+<style scoped>
+#countdown {
+	@apply m-2 text-center;
+	font-size: 0.85em;
+}
+
+#progress-bar {
+	@apply rounded-lg dark:bg-gray;
+	background-color: #e8e8e8;
+	height: 20px;
+}
+
+#after {
+	@apply block h-full rounded-lg bg-gradient-to-r from-secondary to-primary;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,152 +1,68 @@
 <template>
-	<div id="app">
-		<portal to="h1">
-			<ruby>
-				Home
-				<rt>家</rt>
-			</ruby>
-		</portal>
+	<div class="flex flex-col">
+		<h2>Programming is like alchemy and I turn 0s and 1s into things in the internet</h2>
 
-		<section class="flex flex-col pb-5">
-			<h2>Programming is like alchemy and I turn 0s and 1s into things in the internet</h2>
+		<div class="text-center mt-6">
+			<img src="~/assets/images/avatar.png" alt="Avatar" style="height: 300px"/>
+		</div>
 
-			<div id="avatar">
-				<img src="~/assets/images/avatar.png" alt="Avatar"/>
-			</div>
+		<p class="mx-auto">
+			Currently attending a Master's Degree in Informatics and Computing Engineering in <a
+			href="https://sigarra.up.pt/feup/">FEUP</a>
 
-			<p class="mx-auto">
-				Currently attending a Master's Degree in Informatics and Computing Engineering in
-				<a href="https://sigarra.up.pt/feup/">FEUP</a>
+			<br/>
 
-				<br/>
+			Interested in
 
-				Interested in
-				<nuxt-link to="/projects">
-					<ruby class="under">
-						building things<rt>事業</rt>
-					</ruby>
-				</nuxt-link><!--
-				-->,
-				<nuxt-link to="/travel">
-					<ruby class="under">
-						travelling<rt>旅行</rt>
-					</ruby>
-				</nuxt-link><!--
-				-->,
-				<nuxt-link to="/music">
-					<ruby class="under">
-						music<rt>音楽</rt>
-					</ruby>
-				</nuxt-link><!--
-				-->,
-				<nuxt-link to="/gaming">
-					<ruby class="under">
-						gaming<rt>ゲーム</rt>
-					</ruby>
-				</nuxt-link>
-				and
-				<nuxt-link to="/anime">
-					<ruby class="under"
-					>anime
-						<rt>アニメ</rt>
-					</ruby>
-					&
-					<ruby class="under"
-					>manga
-						<rt>漫画</rt>
-					</ruby>
-				</nuxt-link>
+			<span v-for="(topic, i) in topics" :key="i" style="display: inline-block">
+			<span v-if="i === topics.length - 1">&nbsp;and </span><span v-else-if="i > 0">, </span>
 
-				<br/>
+			<NuxtLink :to="topic['endpoint']">
+				<ruby class="under">
+					{{ topic.text }}
+					<rt>{{ topic.kana }}</rt>
+				</ruby>
+			</NuxtLink>
+		</span>
+		</p>
 
-				<!--				I also have a-->
-				<!--				<nuxt-link to="/blog">-->
-				<!--					<ruby class="under">blog-->
-				<!--						<rt>網誌</rt>-->
-				<!--					</ruby>-->
-				<!--				</nuxt-link>-->
-			</p>
-
-			<div id="university-progress">
-				<progress-bar :width="width" id="progress-bar"/>
-				<countdown :end_date="end_date"/>
-			</div>
-		</section>
+		<div class="mt-4 w-7/12 mx-auto">
+			<UniversityCountdown/>
+		</div>
 	</div>
 </template>
 
-<script>
-import ProgressBar from "@/components/ProgressBar";
-import {Portal} from "portal-vue";
+<script lang="ts" setup>
+definePageMeta({
+	title: "Home",
+	kana: "家"
+})
 
-const start_date = new Date(2018, 9, 8);
-const end_date = new Date(2023, 6, 21);
-const today = new Date();
-
-export default {
-	head: {
-		title: "Home | Eduardo Correia",
-		meta: [
-			{name: "twitter:title", content: "Home | Eduardo Correia"},
-			{property: "og:title", content: "Home | Eduardo Correia"},
-		],
+const topics = [
+	{
+		endpoint: "projects",
+		text: "building things",
+		kana: "事業"
 	},
-	components: {
-		ProgressBar,
-		Portal,
+	{
+		endpoint: "travel",
+		text: "travelling",
+		kana: "旅行"
 	},
-	data() {
-		return {
-			width: this.calculatePercentage(),
-			start_date: start_date,
-			end_date: end_date,
-			today: today,
-		};
+	{
+		endpoint: "music",
+		text: "music",
+		kana: "音楽"
 	},
-	methods: {
-		daysBetween(date1, date2) {
-			// The number of milliseconds in one day
-			const ONE_DAY = 1000 * 60 * 60 * 24;
-
-			// Convert both dates to milliseconds
-			const date1_ms = date1.getTime();
-			const date2_ms = date2.getTime();
-
-			// Calculate the difference in milliseconds
-			const difference_ms = Math.abs(date1_ms - date2_ms);
-
-			// Convert back to days and return
-			return Math.round(difference_ms / ONE_DAY);
-		},
-		calculatePercentage() {
-			const time1 = this.daysBetween(start_date, end_date);
-			const time2 = this.daysBetween(start_date, today);
-
-			return ((time2 / time1) * 100).toFixed(4);
-		},
+	{
+		endpoint: "gaming",
+		text: "gaming",
+		kana: "ゲーム"
 	},
-};
+	{
+		endpoint: "anime",
+		text: "anime & manga",
+		kana: "アニメ 漫画"
+	}
+]
 </script>
-
-<style scoped>
-#app {
-	font-size: 19px;
-	line-height: 1.5;
-}
-
-#avatar {
-	@apply text-center mt-6;
-}
-
-#avatar img {
-	height: 300px;
-}
-
-#university-progress {
-	@apply flex flex-col h-full flex-1;
-}
-
-#progress-bar {
-	@apply my-2 w-full md:w-7/12 mx-auto;
-}
-</style>
