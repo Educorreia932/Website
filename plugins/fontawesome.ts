73dc3f6f64c8e4ba441c8f1b3440dcdb379e2317
Educorreia932 <skelozard1@gmail.com>
2022-08-14 00:26:24 +0100

    Only adding used icons to Font Awesome's librarys

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,12 +1,11 @@
 import {library, config} from '@fortawesome/fontawesome-svg-core';
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
-import {fas} from '@fortawesome/free-solid-svg-icons';
-import {fab} from "@fortawesome/free-brands-svg-icons";
+import {faLink} from '@fortawesome/free-solid-svg-icons';
+import {faSpotify} from "@fortawesome/free-brands-svg-icons";
 
 config.autoAddCss = false;
 
-library.add(fas);
-library.add(fab);
+library.add(faLink, faSpotify);
 
 export default defineNuxtPlugin((nuxtApp) => {
 	nuxtApp.vueApp.component("FontAwesomeIcon", FontAwesomeIcon, {});
