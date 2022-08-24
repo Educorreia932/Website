18db999a4dbacc29177620044655c8b0d85ea22c
Educorreia932 <skelozard1@gmail.com>
2022-08-24 22:34:23 +0100

    Paused the university countdown

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,11 +1,11 @@
 import {library, config} from '@fortawesome/fontawesome-svg-core';
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
-import {faLink} from '@fortawesome/free-solid-svg-icons';
+import {faLink, faPause} from '@fortawesome/free-solid-svg-icons';
 import {faGithub, faInstagram, faLinkedin, faSpotify} from "@fortawesome/free-brands-svg-icons";
 
 config.autoAddCss = false;
 
-library.add(faLink, faSpotify, faGithub, faInstagram, faLinkedin);
+library.add(faLink, faSpotify, faGithub, faInstagram, faLinkedin, faPause);
 
 export default defineNuxtPlugin((nuxtApp) => {
 	nuxtApp.vueApp.component("FontAwesomeIcon", FontAwesomeIcon, {});
