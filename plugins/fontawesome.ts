a8c670aa6cfc2463070d115e23b5a47042b6a147
Educorreia932 <skelozard1@gmail.com>
2023-01-08 14:35:28 +0900

    Fix: Missing icon

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,11 +1,11 @@
 import {config, library} from '@fortawesome/fontawesome-svg-core';
 import {faGithub, faInstagram, faLinkedin, faSpotify} from "@fortawesome/free-brands-svg-icons";
-import {faArrowUp, faLink, faPause} from '@fortawesome/free-solid-svg-icons';
+import {faArrowUp, faLink, faPause, faVolumeMute} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 
 config.autoAddCss = false;
 
-library.add(faLink, faSpotify, faGithub, faInstagram, faLinkedin, faPause, faArrowUp);
+library.add(faLink, faSpotify, faGithub, faInstagram, faLinkedin, faPause, faArrowUp, faVolumeMute);
 
 export default defineNuxtPlugin((nuxtApp) => {
 	nuxtApp.vueApp.component("FontAwesomeIcon", FontAwesomeIcon);
