5b61c4559b62302042d6f3c73358136308e42a00
Educorreia932 <skelozard1@gmail.com>
2023-01-08 14:23:17 +0900

    Improvements on navigation tabs
    - Style for light mode and responsiveness
    - Disabled highlight color on elements

--- components/NavigationTabs.vue
+++ components/NavigationTabs.vue
@@ -13,16 +13,18 @@
 </template>
 
 <script setup lang="ts">
-let currentIndex = ref(0);
+const route = useRoute();
 
 const pages = [
-	{path: "/", icon: "house"},
-	{path: "projects", icon: "pencil-ruler"},
-	{path: "travel", icon: "plane"},
-	{path: "music", icon: "music"},
-	{path: "gaming", icon: "gamepad"},
-	{path: "anime", icon: "tv"},
+	{title: "Home", path: "/", icon: "house"},
+	{title: "Projects", path: "projects", icon: "pencil-ruler"},
+	{title: "Travel", path: "travel", icon: "plane"},
+	{title: "Music", path: "music", icon: "music"},
+	{title: "Gaming", path: "gaming", icon: "gamepad"},
+	{title: "Anime & Manga", path: "anime", icon: "tv"},
 ];
+
+let currentIndex = ref(pages.findIndex(({title}) => title == route.meta.title));
 </script>
 
 <style scoped>
@@ -31,11 +33,11 @@ nav {
 }
 
 .tab {
-	@apply bg-gray px-5 py-1 text-white rounded-t-lg opacity-50 transition-all;
-	background-color: #363636;
+	@apply bg-[#DCDCDC] dark:bg-[#363636] text-black dark:text-white px-4 md:px-5 py-1 rounded-t-lg opacity-60 transition-all;
+	width: 5vw;
 
 	&.active {
-		@apply bg-gray-dark opacity-100;
+		@apply bg-white dark:bg-gray-dark opacity-100;
 	}
 }
 </style>
